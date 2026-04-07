f29c72a3938f7e044c1a360aefcbc3c0a019b3d5
vinicius franca <vfranca14@gmail.com>
2026-04-06 21:41:13 -0300

    Inserindo dados fakes no GET e criando metodo POST nas rotas de Servico

--- barbearia/API/Program.cs
+++ barbearia/API/Program.cs
@@ -1,9 +1,22 @@
+using API.Models;
+
+var servicos = new List<Servico>
+{
+    new() { Nome = "Barba" },
+    new() { Nome = "Corte" }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/api/servicos", () =>
 {
-    return "Lista de serviços";
+    return servicos;
+});
+
+app.MapPost("/api/servicos", (Servico servico) =>
+{
+    servicos.Add(servico);
 });
 
 app.Run();
