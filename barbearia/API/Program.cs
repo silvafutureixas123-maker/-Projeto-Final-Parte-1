87fcdbd56e32c164e9e2c2863fd285fb04fb8604
vinicius franca <vfranca14@gmail.com>
2026-04-13 20:19:21 -0300

    Adicionando retornos http e validacao de nome no POST

--- barbearia/API/Program.cs
+++ barbearia/API/Program.cs
@@ -1,22 +1,24 @@
 using API.Models;
 
-var servicos = new List<Servico>
-{
-    new() { Nome = "Barba" },
-    new() { Nome = "Corte" }
-};
+var servicos = new List<Servico>();
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/api/servicos", () =>
 {
-    return servicos;
+    return Results.Ok(servicos);
 });
 
 app.MapPost("/api/servicos", (Servico servico) =>
 {
+    if (servicos.Any(s => s.Nome == servico.Nome))
+    {
+        return Results.Conflict("Esse serviço já existe!");
+    }
     servicos.Add(servico);
+
+    return Results.Created("", servico);
 });
 
 app.Run();
