370199798553cbe7300ba0c4d72942e7c0ea1a2e
Pedro Nunes Moreira <pecreeper04@gmail.com>
2026-04-18 16:00:58 -0300

    Alterado tipo da propriedade de situação de int para string

--- barbearia/API/Models/Agendamento.cs
+++ barbearia/API/Models/Agendamento.cs
@@ -6,5 +6,5 @@ public class Agendamento
     public string? IdServico { get; set; }
     public string? IdCliente { get; set; }
     public DateTime DataCadastro { get; set; } = DateTime.Now;
-    public int Situacao { get; set; }
+    public string? Situacao { get; set; }
 }
