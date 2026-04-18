3bdd6a898f08ddbf75d7fc456e769052b46ba24a
vinicius franca <vfranca14@gmail.com>
2026-04-18 15:03:19 -0300

    Removendo entidade de Cliente duplicada

--- barbearia/API/Models/Clientes.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Models;
-
-public class Clientes
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public string? Nome { get; set; }
-    public string? Email { get; set; }
-    public string? Telefone { get; set; }
-}
