894a58d59308432c853a8ebd8b8f14999af0c26b
vinicius franca <vfranca14@gmail.com>
2026-05-02 14:51:28 -0300

    Ajustando obrigatoriedade do campo ID no model de Servico

--- barbearia/API/Models/Servico.cs
+++ barbearia/API/Models/Servico.cs
@@ -2,7 +2,7 @@ namespace API.Models;
 
 public class Servico
 {
-    public string? Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public string? Nome {get; set; }
     public decimal Preco {get; set; }
     public int Duracao {get; set; }
