d4c0d3b2fdfb0f2a70b41b45882fbc95203ae2bb
vinicius franca <vfranca14@gmail.com>
2026-05-02 14:04:18 -0300

    Revert "Adicionando estrutura de clientes"
    This reverts commit 7b1a721cff3b1ed3b8d0640eda76a5e40f8248d4.

--- barbearia/API/Data/AppDataContext.cs
+++ barbearia/API/Data/AppDataContext.cs
@@ -7,7 +7,6 @@ namespace API.Data;
 public class AppDataContext : DbContext
 {
     public DbSet<Servico> Servicos { get; set; }
-    public DbSet<Cliente> Clientes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
