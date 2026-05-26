dc8671c98fc670dde5b83929eed3fa9050b02891
vinicius franca <vfranca14@gmail.com>
2026-05-25 21:13:31 -0300

    Listando servicos retornados pela API

--- barbearia/frontend/src/components/pages/servico/ListarServico.tsx
+++ barbearia/frontend/src/components/pages/servico/ListarServico.tsx
@@ -1,19 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function ListarServicos(){
+function ListarServicos() {
+    const [servicos, setServicos] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:5049/api/servicos")
             .then(resposta => {
                 return resposta.json();
             }).then(dados => {
-                console.log(dados);
+                setServicos(dados);
             });
     }, [])
     
     return (
         <div className="ListarServicos">
             <h1>Componente ListarServico</h1>
-            <table></table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {servicos.map((servico : any) => (
+                        <tr>
+                            <td>{servico.nome}</td>
+                            <td>{servico.preco}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
