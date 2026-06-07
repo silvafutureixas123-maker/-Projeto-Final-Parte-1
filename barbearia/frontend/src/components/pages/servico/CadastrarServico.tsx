7c9a6275aa208a594d6b2209d6fa92175813262c
vinicius franca <vfranca14@gmail.com>
2026-06-07 16:40:37 -0300

    Alterando componente de cadastro de servicos para utilizar a API

--- barbearia/frontend/src/components/pages/servico/CadastrarServico.tsx
+++ barbearia/frontend/src/components/pages/servico/CadastrarServico.tsx
@@ -1,24 +1,55 @@
-import { AnyARecord } from "dns";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Servico from "../../../models/Servico";
+import api from "../../../services/api";
 
 function CadastrarServico() {
     const [nome, setNome] = useState("");
-    
-    function digitar(e : any) {
-        setNome(e.target.value);
-    }
+    const [preco, setPreco] = useState("");
+    const [duracao, setDuracao] = useState("");
 
-    function enviar() {
-        console.log("Servico cadastrado");
+    async function enviarServicoAPI(e : any){
+        e.preventDefault();
+        
+        try {
+            const servico : Servico = {
+                nome,
+                preco: Number(preco),
+                duracao: Number(duracao)
+            };
+
+           const resposta = await api.post("/api/servicos", servico);
+
+            setNome("");
+            setPreco("");
+            setDuracao("");
+
+            console.log(resposta.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
         <div className="CadastrarServico">
             <h1>Cadastrar Serviço</h1>
-            <form onSubmit={enviar}>
+            <form onSubmit={enviarServicoAPI}>
                 <div>
                     <label>Nome: </label>
-                    <input type="text" onChange={digitar}/>
+                    <input value={nome} required type="text" onChange={
+                        (e : any) => {setNome(e.target.value)}
+                    }/>
+                </div>
+                <div>
+                    <label>Preço:</label>
+                    <input value={preco} required type="text" onChange={
+                        (e : any) => {setPreco(e.target.value)}
+                    }/>
+                </div>
+                <div>
+                    <label>Duração:</label>
+                    <input value={duracao} required type="text" onChange={
+                        (e : any) => {setDuracao(e.target.value)}
+                    }/>
                 </div>
                 <div>
                     <button type="submit">
