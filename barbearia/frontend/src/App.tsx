c9a75e5104355c2b35eb78bb28f92b62a448b516
vinicius franca <vfranca14@gmail.com>
2026-06-07 16:42:18 -0300

    Ajustando home da aplicacao e utilizando rotas para funcionalidades

--- barbearia/frontend/src/App.tsx
+++ barbearia/frontend/src/App.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
-
 import ListarServicos from './components/pages/servico/ListarServico';
 import CadastrarServico from './components/pages/servico/CadastrarServico';
-
 import ListaCliente from './components/pages/cliente/ListaCliente';
 import CadastrarCliente from './components/pages/cliente/CadastrarCliente';
-
 import ListarAgendamento from './components/pages/agendamento/ListarAgendamento';
 import CadastrarAgendamento from './components/pages/agendamento/CadastrarAgendamento';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Frontend v1</h1>
-
-      <hr />
-
-      <ListarServicos />
-      <CadastrarServico />
-
-      <hr />
-
-      <ListaCliente />
-      <CadastrarCliente />
-
-      <hr />
-
-<ListarAgendamento />
-<CadastrarAgendamento />
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pages/servico/cadastrar">Cadastrar Serviço</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ListarServicos/>} />
+          <Route path="/pages/servico/cadastrar" element={<CadastrarServico/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
